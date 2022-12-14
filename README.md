cd4e7f1ff7d87d74d3f6e00b09e5262e7388b200
Doronin Stanislav <mugisbrows@gmail.com>
2022-12-14 18:38:58 +0300

    markdown readme

---
+++ README.md
@@ -0,0 +1,24 @@
+# watch-ui 
+
+autocompiling script for ui and qrc files for PySide2 and PyQt5
+
+# Install
+
+watch-ui can be installed via pip as follows:
+
+```
+pip install watch-ui
+```
+
+# Use
+```
+watch-ui /path/to/project
+```
+
+# Author
+
+Stanislav Doronin <mugisbrows@gmail.com>
+
+# License
+
+watch-ui is distributed under the terms of MIT license, check `LICENSE` file.
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-with open('README.rst', encoding='utf-8') as f:
+with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -12,6 +12,7 @@ setup(
     url='https://github.com/mugiseyebrows/watch-ui',
     description='autocompiling script for ui and qrc files for PySide2 and PyQt5',
     long_description = long_description,
+    long_description_content_type = 'text/markdown',
     install_requires = ['eventloop','colorama'],
     entry_points={
         'console_scripts': [
